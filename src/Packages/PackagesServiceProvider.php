466a1b877169a570d2a871422eebde2f4a8fed76
Mahdi Abedi <abedi@jeyserver.com>
2021-06-16 18:11:14 +0430

    Implements DeferrableProvider in packages service provider to improve app performance

--- src/Packages/PackagesServiceProvider.php
+++ src/Packages/PackagesServiceProvider.php
@@ -3,12 +3,19 @@ namespace Jalno\Lumen\Packages;
 
 use Jalno\Lumen\Contracts;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\Support\DeferrableProvider;
 
-class PackagesServiceProvider extends ServiceProvider {
-	public function register()
+class PackagesServiceProvider extends ServiceProvider implements DeferrableProvider
+{
+    public function register()
     {
-		$package = new Repository($this->app);
-		$this->app->instance(Contracts\IPackages::class, $package);
-		$this->app->instance("packages", $package);
+        $package = new Repository($this->app);
+        $this->app->instance(Contracts\IPackages::class, $package);
+        $this->app->instance("packages", $package);
+    }
+
+    public function provides()
+    {
+        return [Contracts\IPackages::class];
     }
 }
