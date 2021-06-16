c337d668f804250db2177fabfd45624ee21688e3
Hossein Hosni <hosni.hossein@gmail.com>
2021-06-16 15:42:44 +0430

    Move getMigrationPaths to it's correct place

--- src/Database/Console/Migrations/MigratePathTrait.php
+++ src/Database/Console/Migrations/MigratePathTrait.php
@@ -4,27 +4,6 @@ namespace Jalno\Lumen\Database\Console\Migrations;
 use Jalno\Lumen\Contracts\IPackage;
 
 trait MigratePathTrait {
-	/**
-     * Get all of the migration paths.
-     *
-     * @return array
-     */
-    protected function getMigrationPaths()
-    {
-        // Here, we will check to see if a path option has been defined. If it has we will
-        // use the path relative to the root of the installation folder so our database
-        // migrations may be run for any customized path from within the application.
-        if ($this->input->hasOption('path') && $this->option('path')) {
-            return collect($this->option('path'))->map(function ($path) {
-                return ! $this->usingRealPath()
-                                ? $this->laravel->basePath().'/'.$path
-                                : $path;
-            })->all();
-        }
-        return array_merge(
-            $this->migrator->paths(), $this->dependenciesMigrationPath(), [$this->getMigrationPath()]
-        );
-    }
 
     protected function dependenciesMigrationPath(?IPackage $package = null): array
     {
